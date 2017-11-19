15ad8247ae7a2b12f7bab91d8a139db58cac5a25
Craig Houston <craighouston01@gmail.com>
2017-11-19 23:18:27 +0000

    Addition on TravisCI config and unit tests

--- knowledge/share/tests.py
+++ knowledge/share/tests.py
@@ -1,3 +1,13 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 # Create your tests here.
+class IndexTests(TestCase):
+
+    def setUp(self):
+        self.thing = "Thing"
+
+    def test_index_view_status_code(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
